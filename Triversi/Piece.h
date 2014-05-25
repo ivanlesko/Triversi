a4f36a892ebb57558bb260666b61592d3bd5763f
ivanlesko <mail@ivanlesko.com>
2014-05-25 16:57:25 -0700

    Added some docs to the Piece class.

--- Triversi/Piece.h
+++ Triversi/Piece.h
@@ -12,6 +12,21 @@
 
 @class Board;
 
+/**
+ *  A Piece object is a view object that also contains the data information about a given piece.
+ *  It draws itself and contains information about itself such as its coordinates on the board.
+ *  Pieces can only be "placed" on a board using the @p placePieceAtRow:@p method.
+ *
+ 
+ @code
+ Piece *newPiece = [Piece placePieceAtRow:touchedPiece.row
+                                andColumn:touchedPiece.column
+                               atPosition:touchedPiece.position
+                            withPieceType:kTRTrianglePieceTypeBlue
+                            withDirection:touchedPiece.direction
+                                withBoard:self.board];
+ @endcode
+ */
 @interface Piece : SKSpriteNode
 
 @property (nonatomic, strong) UIBezierPath *touchableArea;
@@ -23,13 +38,27 @@
 /// Every piece belongs to a board.
 @property (nonatomic, strong) Board *board;
 
+/**
+ *  The piece's neighbor's.  Will contain either one or two other pieces, depending if it is on an edge or not.
+ */
 @property (nonatomic, strong) NSMutableArray *adjacentPieces;
 @property (nonatomic) PieceIndex *leftPiece;
 @property (nonatomic) PieceIndex *rightPiece;
 @property (nonatomic) PieceIndex *topPiece;
 @property (nonatomic) PieceIndex *bottomPiece;
 
-/// Places a new piece on the board with a given row, column, position, type, and direction.
+/**
+ *  Places a new pieces on the board.
+ *
+ *  @param row       the x coordinate (NSNumber)
+ *  @param column    the y coordinate (NSNumber)
+ *  @param position  the pieces position in its parent's coordinate system (CGPoint)
+ *  @param pieceType player1, player2, or neutral
+ *  @param direction up or down
+ *  @param board     the board the pieces resides on
+ *
+ *  @return A new piece
+ */
 + (Piece *)placePieceAtRow:(NSNumber *)row
             andColumn:(NSNumber *)column
            atPosition:(CGPoint)position
